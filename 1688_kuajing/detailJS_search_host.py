f370cf9090914cef5601510b886c5fc3beb8c7d0
LiuShengCheng <yuchajia@gmail.com>
2023-04-08 15:29:19 +0800

    Update and rename detailJS_search.py to detailJS_search_host.py

--- 1688_kuajing/detailJS_search_host.py
+++ 1688_kuajing/detailJS_search_host.py
@@ -11,7 +11,7 @@ import pymysql
 '''format=%(asctime)s具体时间 %(filename)s文件名 %(lenvelname)s日志等级 %(message)s具体信息
    datemt=%a星期 %d日期 %b月份 %Y年份 %H:%M:%S时间'''
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(filename)s %(levelname)s %(message)s',
-                    datefmt='%a %d %b %Y %H:%M:%S', filename='1688test.log', filemode='w')
+                    datefmt='%a %d %b %Y %H:%M:%S', filename='1688_search_host.log', filemode='w')
 
 conn = pymysql.connect(
     host="localhost",
@@ -28,13 +28,6 @@ option.add_argument('lang=zh_CN.UTF-8')
 option.add_experimental_option("excludeSwitches", ['enable-automation', 'enable-logging'])
 option.add_argument('--disable-blink-features=AutomationControlled')
 driver  =  webdriver.Chrome(executable_path=r"C:\Users\admin\AppData\Local\Programs\Python\Python38\chromedriver.exe",chrome_options=option)
-# driver.get("https://www.1688.com/")
-# time.sleep(3)
-# cookies = 
-# driver.delete_all_cookies()
-# for cookie in cookies:
-#     driver.add_cookie(cookie_dict=cookie)
-# time.sleep(3)
 
 
 goodName = input("请输入需要爬取的商品名称：")
@@ -49,11 +42,11 @@ for page in range(1,6):
     time.sleep(5)
 
     # 向下滚动
-    count=800
+    count=1000
     for i in range(6):
         driver.execute_script("document.documentElement.scrollTop={}".format(count))
         time.sleep(random.randint(3,4))
-        count+=800
+        count+=1000
 
     # 读取该js文件
     with open('file.js') as f:
@@ -65,38 +58,43 @@ for page in range(1,6):
     time.sleep(random.randint(3,4))
     # 找到执行js后生成的按钮
     driver.find_element(By.ID,'ywg-alibaba-list-btn').click()
+    # 一直到变化的div
+    items = driver.find_elements(By.XPATH, "/html/body/div[1]/div/div[7]/div[4]/div/div/ul/div")
+    print(len(items))
+    logging.info("当前页有%s个数据条列" % len(items))
     num=0
 
     try:
-        for j in range(1,61):
+        for itemIndex in range(len(items)):
             # 标题
-            titel = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[6]/div[4]/div/div/ul/div[{}]/div/div[2]/a/div'.format(j)).text
+            titel = items[itemIndex].find_element(By.XPATH, './div/div[2]/a/div').text
             # 详情地址
-            # goodsurl = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[6]/div[4]/div/div/ul/div[{}]/div/div[2]/a'.format(j)).get_attribute("href")
-            # goodsurl = goodsurl.replace('/\?.*/', '')
-            # print(goodsurl)
-            # goodsUrlList.append(goodsurl)
+            goodsurl = items[itemIndex].find_element(By.XPATH, './div/div[2]/a').get_attribute("href")
             try:
                 # 复购率
-                repurchase = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[6]/div[4]/div/div/ul/div[{}]/div/div[4]/div/span'.format(j)).text
+                repurchase = items[itemIndex].find_element(By.XPATH, './div/div[4]/div/span').text
+                # repurchase = repurchase[3:]
+                # print(repurchase)
             except Exception as error:
                 print(error)
                 logging.error("错误是%s" % error)
                 repurchase = "暂无复购率"
             # 价格
-            price = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[6]/div[4]/div/div/ul/div[{}]/div/div[5]/div[1]/div[2]'.format(j)).text
+            price = items[itemIndex].find_element(By.XPATH, './div/div[5]/div[1]/div[2]').text + "元"
             # 成交量
-            deal = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[6]/div[4]/div/div/ul/div[{}]/div/div[5]/div[2]/div'.format(j)).text
+            deal = items[itemIndex].find_element(By.XPATH, './div/div[5]/div[2]/div').text
+            # deal = deal[2:-2]
+            # print(deal)
             # 公司名称
-            companyName = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[6]/div[4]/div/div/ul/div[{}]/div/div[6]/div[2]/a/div'.format(j)).text
+            companyName = items[itemIndex].find_element(By.XPATH, './div/div[6]/div[2]/a/div').text
             # 评价和收藏
-            pinjia = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[6]/div[4]/div/div/ul/div[{}]/span'.format(j)).text
+            pinjia = items[itemIndex].find_element(By.XPATH, './span').text
             pinjia = pinjia.replace("璇勪环", "评价").replace("鏀惰棌", "收藏")
             # 正则分离
             match = re.findall(r'评价:(\d+),收藏:(\d+)',pinjia)
             evaluate = match[0][0]
             collect = match[0][1]
-            print(evaluate,collect)
+            #print(evaluate,collect)
 
             num += 1
             createdTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
@@ -105,6 +103,7 @@ for page in range(1,6):
                 "search_key": goodName,
                 "company_name": companyName,
                 "good_subject": titel,
+                "offer_url": goodsurl,
                 "price": price,
                 "deal": deal,
                 "buyback_rate": repurchase,
@@ -113,8 +112,8 @@ for page in range(1,6):
                 "create_time": createdTime
             }
 
-            sql = "insert into 1688_key_search (`search_key`, `company_name`, `good_subject`, `price`, `deal`, `buyback_rate`, `evaluate`, `collect`,`create_time`) \
-                        values ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s','%s')" % (item["search_key"], item["company_name"], item["good_subject"], item["price"],
+            sql = "insert into 1688_key_search (`search_key`, `company_name`, `good_subject`, `offer_url`,`price`, `deal`, `buyback_rate`, `evaluate`, `collect`,`create_time`) \
+                        values ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s','%s','%s')" % (item["search_key"], item["company_name"], item["good_subject"], item["offer_url"],item["price"],
                             item["deal"], item["buyback_rate"], item["evaluate"], item["collect"], item["create_time"])
             cursor.execute(sql)
             conn.commit()
@@ -135,8 +134,7 @@ for page in range(1,6):
     logging.info("爬完了第%s页" % page)
     logging.info("爬到了%s条商品信息" % num)
 
+driver.quit()
 conn.close()
 end = time.time()
-total = end-start
-print(total)
-logging.info("累计耗时%s" % total)
+logging.info("累计耗时%s" %(end-start))
